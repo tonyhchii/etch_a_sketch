caeb2cead7e6a23c363ac19204e169ac1d46a658
Tony Huang <tonyhchii@gmail.com>
2024-08-02 18:35:49 -0400

    added change grid functionality,
    container is now reset and changed to whatever size
    dimension is entered after pressing button

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
 const container = document.querySelector(".container");
+const changeSizeButton = document.querySelector("#changeSize");
 
-for (let i = 0; i < 16; i++){
-    let content = document.createElement("div");
-    content.addEventListener('mouseover', function() {
-        colorChange(content);
-    });
-    container.append(content);
-}
+changeGridSize()
+
 
 function colorChange(content) {
-    content.style.backgroundColor = "red";
+    content.style.backgroundColor = "black";
 }
 
+changeSizeButton.addEventListener("click", changeGridSize);
+
+function changeGridSize() {
+    let squareNum = parseInt(prompt("Enter number of squares between 1-100"));
+    if (!Number.isInteger(squareNum) || squareNum < 0 || squareNum > 100) {
+        alert("Invalid Entry");
+        return;
+    }
+    container.innerHTML = "";
+    for (let i = 0; i < squareNum; i++){
+        for (let j = 0; j < squareNum; j++) {
+            let content = document.createElement("div");
+            content.addEventListener('mouseover', function() {
+            colorChange(content);
+            });
+            container.append(content);
+        }
+    }
+}
